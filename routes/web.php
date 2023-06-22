80ae6075f3bd3fe11dd240a170e56352fd859e60
Luis <fernando2001th@gmail.com>
2023-06-21 21:20:34 -0400

    Implementacion de auth

--- routes/web.php
+++ routes/web.php
@@ -19,28 +19,21 @@ Route::get('/', function () {
     return view('auth.login');
 });
 
-/* Route::get('/producto', function () {
-    return view('producto.index');
-});
+Route::resource('producto', ProductoController::class) -> middleware('auth');
+Route::resource('carro', BoletaController::class) -> middleware('auth');
 
-Route::get('/producto/create',[ProductoController::class,'create']);
-*/
+Auth::routes([]);
+Route::get('/home', [ProductoController::class, 'index'])->name('home');
 
 
-Auth::routes();
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('/', [ProductoController::class, 'index'])->name('home');
+    Route::get('carro', [ProductoController::class, 'carro'])->name('carro');
 
-Route::get('/', [ProductoController::class, 'index'])->name('home');
-
-//Route::group(['middleware' => 'auth.users'], function() {
-
-    Route::resource('producto', ProductoController::class);
-    Route::resource('carro', BoletaController::class);
-
-    Route::get('/home', [ProductoController::class, 'index'])->name('home');
     Route::get('añadir_al_carrito/{id}', [ProductoController::class, 'añadirCarrito'])->name('añadir_al_carrito');
     Route::delete('remove-from-cart', 'ProductoController@remove');
-    Route::get('carro', [ProductoController::class, 'carro'])->name('carro');
+});
+
+
     
-//});
-
-
+    
